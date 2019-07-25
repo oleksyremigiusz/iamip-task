573831f0d7372db163f19dd8f9e653a7af880cd1
oleksyremigiusz <oleksyremigiusz@gmail.com>
2019-07-25 15:18:43 +0200

    unfinished show comments

--- reactapi/src/App.js
+++ reactapi/src/App.js
@@ -3,13 +3,20 @@ import React, { Component } from 'react';
 class App extends Component {
   
     constructor(props) {
-    super(props);
-    this.state = {
-      posts: [],
-      comments: [],
-      isLoaded: false
+      super(props);
+      this.state = {
+        posts: [],
+        comments: [],
+        isLoaded: false,
+        showComment: []
+      }
     }
-  }
+  
+    toggleComment(id) {
+      this.setState({
+        showComment : !this.state.showComment
+      });
+    }
   
   async componentDidMount() {
     Promise.all([
@@ -23,10 +30,17 @@ class App extends Component {
           isLoaded: true
         }));
     }
-  
+
   render() {
 
     var { isLoaded, posts, comments } = this.state;
+    var shownComment = {
+			display: this.state.showComment ? "block" : "none"
+		};
+		
+		var hiddenComment = {
+			display: this.state.showComment ? "none" : "block"
+		}
 
     if (!isLoaded) {
       return <div>Loading...</div>
@@ -35,16 +49,14 @@ class App extends Component {
   
       return ( 
         <div className="App">
-          <div> {posts.map(post => (
-            <div>
+          {posts.map(post => (
+              <div>
               <h1>{post.title}</h1>
-              {post.body}
-              <h2>{comments[1].body}</h2>
-            </div>
-            
+              <h4 name={post.id} style={ shownComment }>{comments[post.id].body}</h4>
+				      <button className={post.id} style={ hiddenComment } onClick={this.toggleComment.bind(this[post.id])}>Pokaż komentarze</button>
+              </div>
           ))}
           </div>
-        </div>
       );
     }
   }
